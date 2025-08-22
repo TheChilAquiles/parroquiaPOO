f8d5e1fa169f1bb312bf769243fe8bf5fa3421dd
luneto1210 <luneto2005a@gmail.com>
2025-08-21 19:38:17 -0500

    V-1.6.4

--- parroquia.sql
+++ parroquia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-08-2025 a las 02:34:56
+-- Tiempo de generación: 20-08-2025 a las 04:53:21
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `parroquia`
+-- Base de datos: `proyectoparroquia`
 --
 
 -- --------------------------------------------------------
@@ -47,25 +47,12 @@ CREATE TABLE `certificado` (
 
 CREATE TABLE `feligreses` (
   `id` bigint(20) NOT NULL,
-  `usuario_id` bigint(20) NOT NULL,
-  `primer_nombre` varchar(255) NOT NULL,
-  `segundo_nombre` varchar(255) NOT NULL,
-  `primer_apellido` varchar(255) NOT NULL,
-  `segundo_apellido` varchar(255) NOT NULL,
-  `tipo_documento_id` bigint(20) NOT NULL,
-  `numero_documento` varchar(255) NOT NULL,
-  `telefono` varchar(255) NOT NULL,
-  `direccion` varchar(255) NOT NULL
+  `usuario_id` bigint(20) DEFAULT NULL,
+  `tipo_documento_id` bigint(20) DEFAULT NULL,
+  `numero_documento` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `feligreses`
---
-
-INSERT INTO `feligreses` (`id`, `usuario_id`, `primer_nombre`, `segundo_nombre`, `primer_apellido`, `segundo_apellido`, `tipo_documento_id`, `numero_documento`, `telefono`, `direccion`) VALUES
-(16, 1, 'asd', '12', 'asd', '123', 1, '1546145', '3111111', 'sad asd asd assd '),
-(17, 15, 'asd', '12', 'as', 'asd', 1, '1546145', '30155', 'sad asd asd assd '),
-(18, 16, 'rusbel ', 'asdasdasd', 'aadasd', 'sadasd', 1, '2154654', '123123123', 'calle 24 norte');
 
 -- --------------------------------------------------------
 
@@ -75,7 +62,7 @@ INSERT INTO `feligreses` (`id`, `usuario_id`, `primer_nombre`, `segundo_nombre`,
 
 CREATE TABLE `grupos` (
   `id` bigint(20) NOT NULL,
-  `nombre` varchar(255) NOT NULL
+  `nombre` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -86,16 +73,16 @@ CREATE TABLE `grupos` (
 
 CREATE TABLE `grupo_roles` (
   `id` bigint(20) NOT NULL,
-  `rol` varchar(255) NOT NULL
+  `rol` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `libros`
+-- Estructura de tabla para la tabla `libro`
 --
 
-CREATE TABLE `libros` (
+CREATE TABLE `libro` (
   `id` bigint(20) NOT NULL,
   `libro_tipo_id` bigint(20) DEFAULT NULL,
   `numero` int(11) DEFAULT NULL,
@@ -103,55 +90,6 @@ CREATE TABLE `libros` (
   `folio` int(11) DEFAULT NULL,
   `fecha_generacion` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `libros`
---
-
-INSERT INTO `libros` (`id`, `libro_tipo_id`, `numero`, `acta`, `folio`, `fecha_generacion`) VALUES
-(1, 2, 1, NULL, NULL, NULL),
-(2, 1, 1, NULL, NULL, NULL),
-(3, 1, 2, NULL, NULL, NULL),
-(4, 3, 1, NULL, NULL, NULL),
-(5, 4, 1, NULL, NULL, NULL),
-(6, 4, 2, NULL, NULL, NULL),
-(7, 4, 3, NULL, NULL, NULL),
-(8, 1, 3, NULL, NULL, NULL),
-(9, 3, 2, NULL, NULL, NULL),
-(10, 3, 3, NULL, NULL, NULL),
-(11, 3, 4, NULL, NULL, NULL),
-(12, 3, 5, NULL, NULL, NULL),
-(13, 3, 6, NULL, NULL, NULL),
-(14, 3, 7, NULL, NULL, NULL),
-(15, 3, 8, NULL, NULL, NULL),
-(16, 3, 9, NULL, NULL, NULL),
-(17, 3, 10, NULL, NULL, NULL),
-(18, 3, 11, NULL, NULL, NULL),
-(19, 3, 12, NULL, NULL, NULL),
-(20, 3, 13, NULL, NULL, NULL),
-(21, 3, 14, NULL, NULL, NULL),
-(22, 3, 15, NULL, NULL, NULL),
-(23, 3, 16, NULL, NULL, NULL),
-(24, 3, 17, NULL, NULL, NULL),
-(25, 3, 18, NULL, NULL, NULL),
-(26, 3, 19, NULL, NULL, NULL),
-(27, 3, 20, NULL, NULL, NULL),
-(28, 1, 4, NULL, NULL, NULL),
-(29, 1, 5, NULL, NULL, NULL),
-(30, 1, 6, NULL, NULL, NULL),
-(31, 1, 7, NULL, NULL, NULL),
-(32, 1, 8, NULL, NULL, NULL),
-(33, 1, 9, NULL, NULL, NULL),
-(34, 1, 10, NULL, NULL, NULL),
-(35, 1, 11, NULL, NULL, NULL),
-(36, 1, 12, NULL, NULL, NULL),
-(37, 1, 13, NULL, NULL, NULL),
-(38, 1, 14, NULL, NULL, NULL),
-(39, 1, 15, NULL, NULL, NULL),
-(40, 1, 16, NULL, NULL, NULL),
-(41, 1, 17, NULL, NULL, NULL),
-(42, 1, 18, NULL, NULL, NULL),
-(43, 1, 19, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -163,14 +101,6 @@ CREATE TABLE `libro_tipo` (
   `id` bigint(20) NOT NULL,
   `tipo` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `libro_tipo`
---
-
-INSERT INTO `libro_tipo` (`id`, `tipo`) VALUES
-(1, 'b'),
-(2, 'c');
 
 -- --------------------------------------------------------
 
@@ -265,33 +195,12 @@ CREATE TABLE `sacramento_tipo` (
 
 CREATE TABLE `usuarios` (
   `id` bigint(20) NOT NULL,
-  `usuario_rol_id` bigint(20) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `email_confirmed` tinyint(1) NOT NULL DEFAULT 0,
-  `contraseña` varchar(255) NOT NULL,
-  `datos_completos` tinyint(1) NOT NULL DEFAULT 0
+  `usuario_rol_id` bigint(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `email_confirmed` tinyint(1) DEFAULT NULL,
+  `contraseña` varchar(255) DEFAULT NULL,
+  `datos_completos` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `usuario_rol_id`, `email`, `email_confirmed`, `contraseña`, `datos_completos`) VALUES
-(1, 1, 'ejemplo2@example.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 1),
-(3, 1, 'ejemploo2@example.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(4, 1, 'ejemploo22@example.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(5, 1, 'ejemploo222@example.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(6, 1, 'ejemploo2227@example.com', 0, '2b44928ae11fb9384c4cf38708677c48', 0),
-(7, 1, 'ejemploo22272@example.com', 0, '2b44928ae11fb9384c4cf38708677c48', 0),
-(8, 1, 'admin@beehive.com', 0, 'c20ad4d76fe97759aa27a0c99bff6710', 0),
-(9, 1, 'admin2@beehive.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(10, 1, 'admin21@beehive.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(11, 1, 'rusbel199@hotmail.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(12, 1, 'admin1@beehive.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(13, 1, 'ejemplo@example.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(14, 1, 'rusbel19955@hotmail.com', 0, '6512bd43d9caa6e02c990b0a82652dca', 0),
-(15, 1, 'admin22@beehive.com', 0, '81dc9bdb52d04dc20036dbd8313ed055', 1),
-(16, 1, 'jrobgal@gmail.com', 0, '81dc9bdb52d04dc20036dbd8313ed055', 1);
 
 -- --------------------------------------------------------
 
@@ -301,9 +210,9 @@ INSERT INTO `usuarios` (`id`, `usuario_rol_id`, `email`, `email_confirmed`, `con
 
 CREATE TABLE `usuario_grupos` (
   `id` bigint(20) NOT NULL,
-  `usuario_id` bigint(20) NOT NULL,
-  `grupo_parroquial_id` bigint(20) NOT NULL,
-  `grupo_rol_id` bigint(20) NOT NULL
+  `usuario_id` bigint(20) DEFAULT NULL,
+  `grupo_parroquial_id` bigint(20) DEFAULT NULL,
+  `grupo_rol_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -314,7 +223,7 @@ CREATE TABLE `usuario_grupos` (
 
 CREATE TABLE `usuario_roles` (
   `id` bigint(20) NOT NULL,
-  `rol` varchar(255) NOT NULL
+  `rol` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -335,7 +244,7 @@ ALTER TABLE `certificado`
 --
 ALTER TABLE `feligreses`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `usuario_id` (`usuario_id`);
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `grupos`
@@ -350,9 +259,9 @@ ALTER TABLE `grupo_roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `libros`
+-- Indices de la tabla `libro`
 --
-ALTER TABLE `libros`
+ALTER TABLE `libro`
   ADD PRIMARY KEY (`id`),
   ADD KEY `libro_tipo_id` (`libro_tipo_id`);
 
@@ -418,7 +327,6 @@ ALTER TABLE `sacramento_tipo`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
   ADD KEY `usuario_rol_id` (`usuario_rol_id`);
 
 --
@@ -426,9 +334,9 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `usuario_grupos`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `grupo_parroquial_id` (`grupo_parroquial_id`),
-  ADD UNIQUE KEY `usuario_id` (`usuario_id`),
-  ADD UNIQUE KEY `grupo_rol_id` (`grupo_rol_id`);
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `grupo_parroquial_id` (`grupo_parroquial_id`),
+  ADD KEY `grupo_rol_id` (`grupo_rol_id`);
 
 --
 -- Indices de la tabla `usuario_roles`
@@ -450,7 +358,7 @@ ALTER TABLE `certificado`
 -- AUTO_INCREMENT de la tabla `feligreses`
 --
 ALTER TABLE `feligreses`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `grupos`
@@ -465,16 +373,16 @@ ALTER TABLE `grupo_roles`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `libros`
+-- AUTO_INCREMENT de la tabla `libro`
 --
-ALTER TABLE `libros`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+ALTER TABLE `libro`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `libro_tipo`
 --
 ALTER TABLE `libro_tipo`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `pago`
@@ -513,10 +421,16 @@ ALTER TABLE `sacramento`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `sacramento_tipo`
+--
+ALTER TABLE `sacramento_tipo`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario_grupos`
@@ -535,10 +449,67 @@ ALTER TABLE `usuario_roles`
 --
 
 --
+-- Filtros para la tabla `certificado`
+--
+ALTER TABLE `certificado`
+  ADD CONSTRAINT `certificado_ibfk_1` FOREIGN KEY (`usuario_generador_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `certificado_ibfk_2` FOREIGN KEY (`feligres_certificado_id`) REFERENCES `feligreses` (`id`),
+  ADD CONSTRAINT `certificado_ibfk_3` FOREIGN KEY (`sacramento_id`) REFERENCES `sacramento` (`id`);
+
+--
+-- Filtros para la tabla `feligreses`
+--
+ALTER TABLE `feligreses`
+  ADD CONSTRAINT `feligreses_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `libro`
+--
+ALTER TABLE `libro`
+  ADD CONSTRAINT `libro_ibfk_1` FOREIGN KEY (`libro_tipo_id`) REFERENCES `libro_tipo` (`id`);
+
+--
+-- Filtros para la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD CONSTRAINT `pago_ibfk_1` FOREIGN KEY (`certificado_id`) REFERENCES `certificado` (`id`);
+
+--
+-- Filtros para la tabla `pariente`
+--
+ALTER TABLE `pariente`
+  ADD CONSTRAINT `pariente_ibfk_1` FOREIGN KEY (`parentesco_id`) REFERENCES `parentezcos` (`id`),
+  ADD CONSTRAINT `pariente_ibfk_2` FOREIGN KEY (`feligres_sujeto_id`) REFERENCES `feligreses` (`id`),
+  ADD CONSTRAINT `pariente_ibfk_3` FOREIGN KEY (`feligres_pariente_id`) REFERENCES `feligreses` (`id`);
+
+--
+-- Filtros para la tabla `participantes`
+--
+ALTER TABLE `participantes`
+  ADD CONSTRAINT `participantes_ibfk_1` FOREIGN KEY (`feligres_id`) REFERENCES `feligreses` (`id`),
+  ADD CONSTRAINT `participantes_ibfk_2` FOREIGN KEY (`sacramento_id`) REFERENCES `sacramento` (`id`),
+  ADD CONSTRAINT `participantes_ibfk_3` FOREIGN KEY (`rol_participante_id`) REFERENCES `participantes_rol` (`id`);
+
+--
+-- Filtros para la tabla `sacramento`
+--
+ALTER TABLE `sacramento`
+  ADD CONSTRAINT `sacramento_ibfk_1` FOREIGN KEY (`libro_id`) REFERENCES `libro` (`id`),
+  ADD CONSTRAINT `sacramento_ibfk_2` FOREIGN KEY (`tipo_sacramento_id`) REFERENCES `sacramento_tipo` (`id`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`usuario_rol_id`) REFERENCES `usuario_roles` (`id`);
+
+--
 -- Filtros para la tabla `usuario_grupos`
 --
 ALTER TABLE `usuario_grupos`
-  ADD CONSTRAINT `usuario_grupos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+  ADD CONSTRAINT `usuario_grupos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `usuario_grupos_ibfk_2` FOREIGN KEY (`grupo_parroquial_id`) REFERENCES `grupos` (`id`),
+  ADD CONSTRAINT `usuario_grupos_ibfk_3` FOREIGN KEY (`grupo_rol_id`) REFERENCES `grupo_roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
